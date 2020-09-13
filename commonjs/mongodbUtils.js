f2bb8a7c80d88cae871bf9c57f06c8e2fd0c49d6
Galileo01 <1354541676@qq.com>
2020-09-13 15:54:27 +0800

    .......

--- commonjs/mongodbUtils.js
+++ commonjs/mongodbUtils.js
@@ -13,9 +13,9 @@ function connect() {
     //本地测试
     // mongoose.connect('mongodb://127.0.0.1/campus_cat_book', { useUnifiedTopology: true, useNewUrlParser: true });
     //服务器环境需要身份验证
-    mongoose.connect('mongodb://admin:pass@127.0.0.1/campus_cat_book?authSource=admin', { useUnifiedTopology: true, useNewUrlParser: true });
+    mongoose.connect('mongodb://admin:15823413506@127.0.0.1/campus_cat_book?authSource=admin', { useUnifiedTopology: true, useNewUrlParser: true });
     //测试连接 远程 服务器 mongodb
-  
+    // mongoose.connect('mongodb://admin:15823413506@121.41.225.12/campus_cat_book?authSource=admin', { useUnifiedTopology: true, useNewUrlParser: true });
     // console.log(mongoose.connection.base);
     // const { modelSchemas } = mongoose.connection.base;
     // for(const prop in modelSchemas)
